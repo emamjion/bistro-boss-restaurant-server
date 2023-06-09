420f7d08feba72092f7b190a0578c00d03bda42a
emamjion <emamjion@gmail.com>
2023-06-09 17:53:40 +0600

    Bistro boss journey finished

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     // All collection is here
     const menuCollection = client.db('bistroDB').collection('menu');
